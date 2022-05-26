7c17f75e4e1ce4126504cd04e5be60d7328fba14
Mariano Casa <marianoqnk@gmail.com>
2022-05-26 07:50:33 +0200

    Fin

---
+++ EstrucutaJSONb.cs
@@ -0,0 +1,41 @@
+﻿
+//Mucho OJO aquí, como hace las importación automática llama a la clase rootobject en todos los casos por lo que puede hacer 
+//confilcto si hay varias importaciones
+public class Rootobjectk
+{
+    public EstrucutaJSONb[] Property1 { get; set; }
+}
+
+public class EstrucutaJSONb
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string username { get; set; }
+    public string email { get; set; }
+    public Address address { get; set; }
+    public string phone { get; set; }
+    public string website { get; set; }
+    public Company company { get; set; }
+}
+
+public class Address
+{
+    public string street { get; set; }
+    public string suite { get; set; }
+    public string city { get; set; }
+    public string zipcode { get; set; }
+    public Geo geo { get; set; }
+}
+
+public class Geo
+{
+    public string lat { get; set; }
+    public string lng { get; set; }
+}
+
+public class Company
+{
+    public string name { get; set; }
+    public string catchPhrase { get; set; }
+    public string bs { get; set; }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿using System.Net.Http.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using System.Net.Http.Json;
+using EstructuraJSON;
+
+
 
 namespace HttpClientExtensionMethods
 {
+    //Previamente hemos visto como se estructuran los datos JSON si no lo tenemos debemos extraerlo de algun sitio
     public class User
     {
         public int Id { get; set; }
@@ -20,20 +30,43 @@ namespace HttpClientExtensionMethods
             };
 
             // Get the user information.
-            List<User>? m= new List<User>(); 
 
-            m = await client.GetFromJsonAsync<List<User>>("users/");
+
+            List<User>? m = await client.GetFromJsonAsync<List<User>>("users/");
             foreach (User user in m)
             {
                 Console.WriteLine($"Id: {user?.Id}");
                 Console.WriteLine($"Name: {user?.Name}");
                 Console.WriteLine($"Username: {user?.Username}");
                 Console.WriteLine($"Email: {user?.Email}");
+                Console.WriteLine();
             }
-            // Post a new user.
-            //HttpResponseMessage response = await client.PostAsJsonAsync("users", user);
-            //Console.WriteLine(
-              //  $"{(response.IsSuccessStatusCode ? "Success" : "Error")} - {response.StatusCode}");
+
+            //Otro ejemplo
+            Console.WriteLine("Pulsa para ver otro ejemplo");
+            Console.Read();
+
+            HttpClient client2 = new()
+            {
+                BaseAddress = new Uri("https://www.floatrates.com")
+                
+            };
+            //using HttpClient client2 = httpClient;
+        
+            Rootobject? k = await client2.GetFromJsonAsync<Rootobject>("daily/eur.json");
+
+            Console.WriteLine($"Code: {k.usd.code}");
+            Console.WriteLine($"AlfaCode: {k.usd.alphaCode}");
+            Console.WriteLine($"NumCode: {k.usd.numericCode}");
+            Console.WriteLine($"Name: {k.usd.name}");
+            Console.WriteLine($"Rate: {k.usd.rate}");
+            Console.WriteLine($"Date: {k.usd.date}");
+            Console.WriteLine($"InvRate: {k.usd.inverseRate}");
+            Console.WriteLine();
+
+            Console.Read();
+
         }
+
     }
 }
